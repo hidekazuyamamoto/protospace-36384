4e240df41af42fb2ee058cda52eaf11dd7a6cf86
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-07-30 17:59:21 +0900

    実装課題　修正（7月30日　18:00）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
 
-  before_action  :move_to_index, only: :edit
+  before_action  :move_to_index_for_edit, only: :edit
+  before_action  :move_to_index_for_new, only: :new
 
   def index
     @prototypes = Prototype.all.includes(:user)
@@ -49,12 +50,22 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    prototype = Prototype.find(params[:id])
-    unless user_signed_in? && prototype.user_id == current_user.id
-      redirect_to root_path
+    def move_to_index_for_new
+      unless user_signed_in?
+        redirect_to root_path
+      end
     end
-  end
+
+    def move_to_index_for_edit
+      if user_signed_in?
+        prototype = Prototype.find(params[:id])
+        unless  prototype.user.id == current_user.id
+          redirect_to root_path
+        end
+      else
+        redirect_to root_path
+      end
+    end
 
 
 end
